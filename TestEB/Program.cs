b5af1942ce6e96558f24521c71d11ca8286ea6cf
faarseer <everforeseeing@gmail.com>
2023-04-06 18:18:12 +0900

    Test0406.008

--- TestEB/Program.cs
+++ TestEB/Program.cs
@@ -1,6 +1,7 @@
 using TestEB.Models;
 using Pomelo.EntityFrameworkCore.MySql;
 using Microsoft.EntityFrameworkCore;
+using ConfigurationManager = System.Configuration.ConfigurationManager;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,7 +12,17 @@ builder.Services.AddControllers();
 // Add Db
 //var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 //var connectionString = Helpers.GetRDSConnectionString();
-var connectionString = "Server=awseb-e-7sevd4snji-stack-awsebrdsdatabase-5xhtsbabrtyq.c4leokrb7cia.ap-northeast-2.rds.amazonaws.com;Port=3306;Uid=admin;pwd=test123!;Database=ebdb;";
+var appConfig = ConfigurationManager.AppSettings;
+
+string dbname = appConfig["RDS_DB_NAME"];
+string username = appConfig["RDS_USERNAME"];
+string password = appConfig["RDS_PASSWORD"];
+string hostname = appConfig["RDS_HOSTNAME"];
+string port = appConfig["RDS_PORT"];
+
+var connectionString= "Server=" + hostname + ";Port=" + port + ";Database=" + dbname + ";Uid=" + username + ";pwd=" + password + ";";
+
+//var connectionString = "Server=awseb-e-7sevd4snji-stack-awsebrdsdatabase-5xhtsbabrtyq.c4leokrb7cia.ap-northeast-2.rds.amazonaws.com;Port=3306;Uid=admin;pwd=test123!;Database=ebdb;";
 var serverVersion = new MySqlServerVersion(new Version(8, 0, 32));
 builder.Services.AddDbContext<TodoContext>(options =>
 {
